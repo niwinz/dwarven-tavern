67a6020d3ce029c67cd91430695a4a2295398d01
Andrey Antukh <niwi@niwi.nz>
2015-12-05 10:36:18 +0200

    Add bouncer and some dependencies exclusions.

--- project.clj
+++ project.clj
@@ -4,17 +4,22 @@
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.189"]
-
+                 [org.clojure/clojurescript "1.7.189"
+                  :exclusions [org.clojure/tools.reader]]
                  [org.slf4j/slf4j-simple "1.7.13"]
+                 [org.clojure/core.async "0.2.374"]
+
                  [funcool/catacumba "0.9.0-SNAPSHOT"]
+
+                  ;; :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [funcool/cats "1.2.0"]
                  [funcool/promesa "0.6.0"]
                  [funcool/beicon "0.2.0"]
                  [funcool/postal "0.2.0"]
 
+                 [bouncer "0.3.3" :exclusions [clj-time]]
                  [rum "0.6.0"]
                  [figwheel-sidecar "0.5.0-2" :scope "test"]]
-  ;; :main ^:skip-aot dwarven-tavern.server.core
+  ;; :main ^:skip-aot dwarven-tavern.server
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
