b903bcde0e2fe2330c73118d120604a47d9c37b7
Andrey Antukh <niwi@niwi.nz>
2015-12-05 16:31:39 +0200

    Move some logic to game and add impl for move message.

--- src/dwarven_tavern/server/api.clj
+++ src/dwarven_tavern/server/api.clj
@@ -7,69 +7,20 @@
             [dwarven-tavern.schema :as schema]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Game
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; NOTE: move to other ns when this section grows
-
-(defn- choice-team
-  [room playerid]
-  (let [team1 (:team1 room)
-        team2 (:team2 room)]
-    (cond
-      (empty? team1) :team1
-      (empty? team2) :team2
-      (contains? team1 playerid) :team1
-      (contains? team2 playerid) :team2
-      (> (count team1) (count team2)) :team2
-      (> (count team2) (count team1)) :team1
-      :else :team1)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; State
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defonce state
   (atom {:rooms {}}))
 
-(defmethod state/transition :assoc-player
-  [state [_ room player]]
-  (let [room' (get-in state [:rooms room])
-        team (choice-team room' player)
-        player' (game/mk-player player team)]
-    (-> state
-        (update-in [:rooms room team] conj player)
-        (update-in [:rooms room :players] assoc player player'))))
-
-(defmethod state/transition :create-room
-  [state [_ roomid]]
-  (let [room (game/mk-room roomid)]
-    (update-in state [:rooms] assoc roomid room)))
-
-(defmethod state/transition :join
-  [state [_ {:keys [room player] :as msg}]]
-  (if-let [room' (get-in state [:rooms room])]
-    (if-let [player' (get-in room' [:players player])]
-      state
-      (state/transition state [:assoc-player room player]))
-    (-> state
-        (state/transition [:create-room room])
-        (state/transition [:assoc-player room player]))))
-
-(defmethod state/transition :start-game
-  [state [_ roomid]]
-  (let [state (update-in state [:rooms roomid] assoc :status :playing)
-        room (get-in state [:rooms roomid])]
-    (game/start room)
-    state))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Helpers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn- in-room?
-  [state roomid playerid]
-  (let [room (get-in state [:rooms roomid :players])]
-    (contains? room playerid)))
+;; (defn- in-room?
+;;   [state roomid playerid]
+;;   (let [room (get-in state [:rooms roomid :players])]
+;;     (contains? room playerid)))
 
 (defn- strip-room
   [room]
@@ -100,32 +51,32 @@
       (swap! state state/transition [:start-game roomid])
       (pc/frame {:ok true}))))
 
-;; (defmethod handler [:novelty :movement]
-;;   [context {:keys [data]}]
-;;   (if-not (schema/valid? schema/+move-msg+ data)
-;;     (pc/frame :error {:message "Invalid message"})
-
-
-(defn on-subscribe
-  [{:keys [out ctrl] :as context}]
-  (let [room (::room context)
-        ch (a/tap (:mult room) (a/chan) true)]
-    (a/go-loop []
-      (let [[val p] (a/alts! [ch ctrl])]
-        (cond
-          (identical? p ctrl)
-          (a/close! ch)
-
-          (identical? p ch)
-          (if (a/>! out val)
-            (recur)
-            (a/close! ch)))))))
+(defmethod handler [:novelty :movement]
+  [context {:keys [data]}]
+  (if-let [errors (schema/validate-move-msg data)]
+    (pc/frame :error errors)
+    (do
+      (swap! state state/transition [:move data])
+      (pc/frame {}))))
 
 (defmethod handler [:subscribe :game]
   [context {:keys [data] :as frame}]
-  (if-let [errors (schema/validate-subscribe-msg data)]
-    (pc/frame :error errors)
-    (let [roomid (:room data)
-          room (get-in @state [:rooms roomid])]
-      (-> (assoc context ::room room)
-          (pc/socket on-subscribe)))))
+  (letfn [(on-subscribe [{:keys [out ctrl] :as context}]
+            (let [room (::room context)
+                  ch (a/tap (:mult room) (a/chan) true)]
+              (a/go-loop []
+                (let [[val p] (a/alts! [ch ctrl])]
+                  (cond
+                    (identical? p ctrl)
+                    (a/close! ch)
+
+                    (identical? p ch)
+                    (if (a/>! out val)
+                      (recur)
+                      (a/close! ch)))))))]
+    (if-let [errors (schema/validate-subscribe-msg data)]
+      (pc/frame :error errors)
+      (let [roomid (:room data)
+            room (get-in @state [:rooms roomid])]
+        (-> (assoc context ::room room)
+            (pc/socket on-subscribe))))))
--- src/dwarven_tavern/server/game.clj
+++ src/dwarven_tavern/server/game.clj
@@ -1,6 +1,11 @@
 (ns dwarven-tavern.server.game
   (:require [clojure.core.async :as a]
-            [catacumba.handlers.postal :as pc]))
+            [catacumba.handlers.postal :as pc]
+            [dwarven-tavern.state :as state]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Basic game components constructors
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def ^:static +default-room-width+ 10)
 (def ^:static +default-room-height+ 10)
@@ -18,6 +23,9 @@
      :id roomid
      :status :pending
      :players {}
+     :movements {}
+     :round 1
+     :start-time nil
      :in in
      :mult mult
      :team1 #{}
@@ -36,6 +44,23 @@
                             (quot +default-room-height+ 2))]
             :team team :dir :north}))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Game logic
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- choice-team
+  [room playerid]
+  (let [team1 (:team1 room)
+        team2 (:team2 room)]
+    (cond
+      (empty? team1) :team1
+      (empty? team2) :team2
+      (contains? team1 playerid) :team1
+      (contains? team2 playerid) :team2
+      (> (count team1) (count team2)) :team2
+      (> (count team2) (count team1)) :team1
+      :else :team1)))
+
 (defn broadcast-start
   [{:keys [in id] :as room}]
   (let [msg {:room id :event :start}]
@@ -48,3 +73,47 @@
     (a/<! (broadcast-start room))
     ;; TODO
     ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; State
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmethod state/transition :assoc-player
+  [state [_ room player]]
+  (let [room' (get-in state [:rooms room])
+        team (choice-team room' player)
+        player' (mk-player player team)]
+    (-> state
+        (update-in [:rooms room team] conj player)
+        (update-in [:rooms room :players] assoc player player'))))
+
+(defmethod state/transition :create-room
+  [state [_ roomid]]
+  (let [room (mk-room roomid)]
+    (update-in state [:rooms] assoc roomid room)))
+
+(defmethod state/transition :join
+  [state [_ {:keys [room player] :as msg}]]
+  (if-let [room' (get-in state [:rooms room])]
+    (if-let [player' (get-in room' [:players player])]
+      state
+      (state/transition state [:assoc-player room player]))
+    (-> state
+        (state/transition [:create-room room])
+        (state/transition [:assoc-player room player]))))
+
+(defmethod state/transition :start-game
+  [state [_ roomid]]
+  (let [state (update-in state [:rooms roomid] merge
+                         {:status :playing
+                          :start-time (System/nanoTime)})
+        room (get-in state [:rooms roomid])]
+    (start room)
+    state))
+
+(defmethod state/transition :move
+  [state [_ data]]
+  (let [roomid (:room data)
+        playerid (:player data)
+        direction (:dir data)]
+    (update-in state [:rooms roomid :moviments] playerid direction)))
