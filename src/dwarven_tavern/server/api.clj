aa268f1fe54aae143f8558a3ac4f6b2ff6bfb5a1
Andrey Antukh <niwi@niwi.nz>
2015-12-05 10:36:35 +0200

    More draft code to server api ns.

--- src/dwarven_tavern/server/api.clj
+++ src/dwarven_tavern/server/api.clj
@@ -1,17 +1,60 @@
 (ns dwarven-tavern.server.api
-  (:require [catacumba.core :as ct]
-            [catacumba.handlers.postal :as pc]))
+  (:require [clojure.core.async :as a]
+            [catacumba.core :as ct]
+            [catacumba.handlers.postal :as pc]
+            [dwarven-tavern.common.state :as state]
+            [dwarven-tavern.server.schema :as schema]
+            ;; [dwarven-tavern.server.game :as game]
+            ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Game
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; NOTE: move to other ns when this section grows
+
+(def +room-statuses+ #{:incomplete
+                       :ready
+                       :playing
+                       :closing})
+
+(defn initialize-room
+  []
+  (let [out (a/chan 8)
+        mix (a/mix out)]
+    {:players []
+     :socket out
+     :mix mix
+     :status :incomplete
+     :responses {}}))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; State
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defonce state
+  (atom {:rooms {}}))
+
+(defmethod state/transition :join-room
+  [state [_ {:keys [name]}]]
+  ;; TODO
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Postal Api
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defmulti handler
   (comp (juxt :type :dest) second vector))
 
-(defmethod handler [:novelty :foobar]
+(defmethod handler [:novelty :join-room]
   [context {:keys [data]}]
   )
 
-(defmethod handler [:subscribe :chats]
-  [context frame]
-  (let [bus (get-in context [::app :eventbus])]
-    (letfn [(on-socket [{:keys [in out ctrl]}]
-              )]
-      (pc/socket context on-socket))))
+;; (defmethod handler [:subscribe :game]
+;;   [context frame]
+;;   (letfn [(on-socket [{:keys [in out ctrl]}]
+;;             )]
+;;     (let [mesage (:data frame)]
+;;       (swap! state/transition [:join-room message]
+;;     (if (
+;;     (pc/socket context on-socket)))
