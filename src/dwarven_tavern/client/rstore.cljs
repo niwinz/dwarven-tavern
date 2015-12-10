32d16b9636e8e2f0ecef2905cb6583a5d66d55f9
Andrey Antukh <niwi@niwi.nz>
2015-12-10 20:53:30 +0200

    Add some comments to the rstore file.

--- src/dwarven_tavern/client/rstore.cljs
+++ src/dwarven_tavern/client/rstore.cljs
@@ -3,17 +3,29 @@
   (:require [beicon.core :as rx]
             [promesa.core :as prom]))
 
+;; An abstraction for implement a simple state
+;; transition. The `-apply-update` function receives
+;; the state and shoudl return the transformed state.
+
 (defprotocol UpdateEvent
-  ;; Event -> Model -> Model
-  (-apply-update [event model]))
+  (-apply-update [event state]))
+
+;; An abstraction for perform some async stuff such
+;; as communicate with api rest or other resources
+;; that implies asynchronous access.
+;; The `-apply-watch` receives the state and should
+;; return a reactive stream of events (that can be
+;; of `UpdateEvent`, `WatchEvent` or `EffectEvent`.
 
 (defprotocol WatchEvent
-  ;; Event -> Model -> Stream[Event]
-  (-apply-watch [event model]))
+  (-apply-watch [event state]))
+
+;; An abstraction for perform just side effects. It
+;; receives state and its return value is completly
+;; ignored.
 
 (defprotocol EffectEvent
-  ;; Event -> Model -> nil
-  (-apply-effect [event model]))
+  (-apply-effect [event state]))
 
 (defn update?
   "Return `true` when `e` satisfies
@@ -36,11 +48,25 @@
 (defonce ^:static bus (rx/bus))
 
 (defn emit!
-  "Emits an event."
+  "Emits an event or a collection of them.
+  The order of events does not matters."
   ([event]
    (rx/push! bus event))
   ([event & events]
    (run! #(rx/push! bus %) (into [event] events))))
+
+(defn swap-state
+  "A helper for just apply some function to state
+  without a need to declare additional event."
+  [f]
+  (reify
+    rs/UpdateEvent
+    (-apply-update [_ state]
+      (f state))
+
+    IPrintWithWriter
+    (-pr-writer [mv writer _]
+      (-write writer "#<event:rstore/swap-state>"))))
 
 (defn reset-state
   "A event that resets the internal state with
@@ -53,7 +79,7 @@
 
     IPrintWithWriter
     (-pr-writer [mv writer _]
-      (-write writer "#<event:state/reset-state>"))))
+      (-write writer "#<event:rstore/reset-state>"))))
 
 (defn init
   "Initializes the stream event loop and
@@ -81,3 +107,41 @@
     (emit! (reset-state state))
 
     state-s))
+
+(comment
+  ;; Import all dependencies.
+  (require '[somewhere.rstore :as rs])
+  (require '[beicon.core :as rx])
+
+  ;; Creates the stream loop with the initial state.
+  ;; The state now, lives as reactive stream and
+  ;; only can be modified emiting events. The returned
+  ;; stream will emit the state snapshots.
+
+  (defn initial-state
+    []
+    {:msg "hello world"})
+
+  (defonce stream (rt/init (initial-state)))
+
+  ;; You can materialize the last state into
+  ;; the atom for using it in the same way as
+  ;; you have done before using any of the
+  ;; react wrappers like rum, reagent or om.
+
+  (defonce state (atom nil))
+  (rx/to-atom stream state)
+
+  ;; You can reinitialize the state completly
+  ;; emiting an already provided defined event:
+
+  (let [state (initial-state)]
+    (rs/emit! (reset-state state)))
+
+  ;; You can transform the state in the same
+  ;; way as you can do it with a plain atom
+  ;; with `swap-state` event:
+
+  (rs/emit! (swap-state #(assoc % :foo "bar")))
+
+  )
